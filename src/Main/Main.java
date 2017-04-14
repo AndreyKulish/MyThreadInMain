b2e78727d1667737a4a48517d71d976a97f36702
HITEK <hitekdada@gmail.com>
2017-04-14 14:52:03 +0300

    Mutix synchronized block curentTime/2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,7 @@ package Main;
 
 
 public class Main{
-
+    Object objectMutix = new Object();
     public volatile static int incrmnt;
 
     public static void main(String[] args) throws InterruptedException {
@@ -10,23 +10,29 @@ public class Main{
         HZ hz = new HZ(main);
             Thread thread1 = new Thread(hz);
             Thread thread2 = new Thread(hz);
+            long onTime = System.currentTimeMillis();
             thread1.start();
             thread2.start();
         System.out.println("Whait...");
         thread1.join();
         thread2.join();
+        long offTime = System.currentTimeMillis();
+        System.out.println("Затраченное время - " + (offTime - onTime));
         System.out.println(main.incrmnt);
 
     }
 
-    synchronized void incr(){
+     void incr(){
+
         for (int i = 0; i < 2000; i++) {
             try {
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            synchronized (objectMutix){
             Main.incrmnt++;
+        }
         }
     }
 
